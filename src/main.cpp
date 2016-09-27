44ab8491e22591580b9b05c9c07c4b386c57795d
Joe Burchett <joe.burchett@gmail.com>
2016-09-26 22:58:27 -0700

    Trying to figure out why YAML node doesn't parse

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,23 @@
 
 int main()
 {
-  YAML::Node config = YAML::LoadFile("config.yaml");
+  YAML::Node node = YAML::Load("token: value");
+  switch (node.Type()){
+    case YAML::NodeType::Null: 
+      std::cout << "Type is Null\n"; 
+    case YAML::NodeType::Scalar: 
+      std::cout << "Type is Scalar\n"; 
+    case YAML::NodeType::Sequence:
+      std::cout << "Type is Sequence\n"; 
+    case YAML::NodeType::Map:
+      std::cout << "Type is Map\n";
+    case YAML::NodeType::Undefined:
+      std::cout << "Type is Undefined\n";
+    default: 
+      std::cout << "ohnoes\n";  //node.Type() hits default case; expect Map
+  } 
+  std::cout << node << std::endl;
+  std::cout << node["token"].as<std::string>() << std::endl; //segfault
+
   return 0;
 }
