16df4c441ee1fb8de927c351154ad9cdc4c358a3
Joe Burchett <joe.burchett@gmail.com>
2016-09-27 08:38:23 -0700

    dederp breaks; strip trailing whitespace.

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,25 @@ int main()
 {
   YAML::Node node = YAML::Load("token: value");
   switch (node.Type()){
-    case YAML::NodeType::Null: 
-      std::cout << "Type is Null\n"; 
-    case YAML::NodeType::Scalar: 
-      std::cout << "Type is Scalar\n"; 
+    case YAML::NodeType::Null:
+      std::cout << "Type is Null\n";
+      break;
+    case YAML::NodeType::Scalar:
+      std::cout << "Type is Scalar\n";
+      break;
     case YAML::NodeType::Sequence:
-      std::cout << "Type is Sequence\n"; 
+      std::cout << "Type is Sequence\n";
+      break;
     case YAML::NodeType::Map:
       std::cout << "Type is Map\n";
+      break;
     case YAML::NodeType::Undefined:
       std::cout << "Type is Undefined\n";
-    default: 
+      break;
+    default:
       std::cout << "ohnoes\n";  //node.Type() hits default case; expect Map
-  } 
+      break;
+  }
   std::cout << node << std::endl;
   std::cout << node["token"].as<std::string>() << std::endl; //segfault
 
