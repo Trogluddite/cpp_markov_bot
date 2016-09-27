81d88f63824caf2a8595761fce865224477081bc
Joe Burchett <joe.burchett@gmail.com>
2016-09-27 15:28:16 -0700

    smartassery

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
 #include "yaml-cpp/yaml.h"
 
 int main()
 {
   YAML::Node config = YAML::LoadFile("../config/config.yaml");
   std::cout << "Your slack token is: " << config["slack_token"].as<std::string>() << std::endl;
+
+  // smartassery
+  printf("I'm a C function! \n");
+  std::printf("I'm a C function wearing a C++ std namespace wrapper!\n");
 
   return 0;
 }
