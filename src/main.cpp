d5dcec959a86d057c7b1e2f9ac3d4fae5f3b70de
Joe Burchett <joe.burchett@gmail.com>
2016-09-27 10:40:30 -0700

    yaml read issues are fixed; recompiled yaml-cpp as a shared lib.

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,8 @@
 
 int main()
 {
-  YAML::Node node = YAML::Load("token: value");
-  switch (node.Type()){
-    case YAML::NodeType::Null:
-      std::cout << "Type is Null\n";
-      break;
-    case YAML::NodeType::Scalar:
-      std::cout << "Type is Scalar\n";
-      break;
-    case YAML::NodeType::Sequence:
-      std::cout << "Type is Sequence\n";
-      break;
-    case YAML::NodeType::Map:
-      std::cout << "Type is Map\n";
-      break;
-    case YAML::NodeType::Undefined:
-      std::cout << "Type is Undefined\n";
-      break;
-    default:
-      std::cout << "ohnoes\n";  //node.Type() hits default case; expect Map
-      break;
-  }
-  std::cout << node << std::endl;
-  std::cout << node["token"].as<std::string>() << std::endl; //segfault
+  YAML::Node config = YAML::LoadFile("../config/config.yaml");
+  std::cout << "Your slack token is: " << config["slack_token"].as<std::string>() << std::endl;
 
   return 0;
 }
